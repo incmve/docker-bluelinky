2b0fcf856dfa33ff487354ed43161fba811cf90d
Philip Wrangles <p.wrangles@gmail.com>
2020-03-20 16:36:20 +0000

    change airTemp for EU, removed irrelevant endpoint

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ app.post('/start', async (req, res) => {
     response = await vehicle.start({
       airCtrl: true,
       igniOnDuration: 10,
-      airTempvalue: 60
+      airTempvalue: 20,
+      defrost: false
     });
   } catch (e) {
     response = {
@@ -77,41 +78,6 @@ app.post('/status', async (req, res) => {
     };
   }
   res.send(response);
-});
-
-// Home Assistant Sensor Integration
-app.get('/hass', async (req, res) => {
-  await checkPin();
-  await myVehicles[0].update();
-  const v = myVehicles[0];
-  res.json({
-      airCtrlOn: v.status.airCtrlOn,
-      latitude: v.location.coord.lat,
-      longitude: v.location.coord.lon,
-      altitude: v.location.coord.alt,
-      speed: v.location.speed.value,
-      doorsLocked: v.status.doorLock,
-      hoodOpen: v.status.hoodOpen,
-      steerWheelHeat: v.status.steerWheelHeat === 1,
-      doorOpenFrontLeft: v.status.doorOpen.frontLeft === 1,
-      doorOpenFrontRight: v.status.doorOpen.frontRight === 1,
-      doorOpenBackLeft: v.status.doorOpen.backLeft === 1,
-      doorOpenBackRight: v.status.doorOpen.backRight === 1,
-      trunkOpen: v.status.trunkOpen,
-      airTemp: v.status.airTemp.value,
-      defrost: v.status.defrost,
-      batteryCharge: v.status.evStatus.batteryCharge,
-      batteryStatus: checkAndSetCharge(v.status.evStatus.batteryStatus),
-      timeToFullCharge: v.status.evStatus.remainTime2[Object.keys(v.status.evStatus.remainTime2)[v.status.evStatus.batteryPlugin]].value,
-      drivingDistance: v.status.evStatus.drvDistance[0].rangeByFuel.totalAvailableRange.value,
-      tirePressureLampAll: v.status.tirePressureLamp.tirePressureLampAll === 1,
-      tirePressureLampFL: v.status.tirePressureLamp.tirePressureLampFL === 1,
-      tirePressureLampFR: v.status.tirePressureLamp.tirePressureLampFR === 1,
-      tirePressureLampRL: v.status.tirePressureLamp.tirePressureLampRL === 1,
-      tirePressureLampRR: v.status.tirePressureLamp.tirePressureLampFR === 1,
-      odometer: v.odometer.value,
-      name: myVehicles[0].name
-  });
 });
 
 app.listen(8080, '0.0.0.0');
